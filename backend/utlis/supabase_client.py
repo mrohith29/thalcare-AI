75e663e366b9c7f31d8880bd51a97caa6951520e
mrohith29 <mariyalarohith29@gmail.com>
2025-08-04 21:16:38 +0530

    feat: added supabase client

---
+++ backend/utlis/supabase_client.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+from supabase import create_client, Client
+
+load_dotenv()
+def get_Supabase():    
+    url: str = os.environ.get("VITE_SUPABASE_URL")
+    key: str = os.environ.get("VITE_SUPABASE_ANON_KEY")
+    supabase: Client = create_client(url, key)
+    return supabase
+
+if __name__ == "__main__":
+    get_Supabase()
